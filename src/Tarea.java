100f996c7480c63a634ed39fbf35939b2611e2e5
Francis Romero Flores <francis.s540482@cesurformacion.com>
2026-09-18 10:10:33 +0200

    Initial commit

---
+++ src/Tarea.java
@@ -0,0 +1,42 @@
+public class Tarea {
+    String nombre;
+    String descripcion;
+    String estado;
+
+    public Tarea(String nombre, String descripcion, String estado) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre +
+                "\nDescripción: " + descripcion +
+                "\nEstado: " + estado;
+    }
+}
